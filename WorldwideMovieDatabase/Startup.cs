9ba5ce68b059dcab110d28f1a9634c328596a05e
Nelson Nyland <nelsonnyland@gmail.com>
2018-10-02 10:54:19 -0700

    Add project files.

---
+++ WorldwideMovieDatabase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorldwideMovieDatabase.Startup))]
+namespace WorldwideMovieDatabase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
