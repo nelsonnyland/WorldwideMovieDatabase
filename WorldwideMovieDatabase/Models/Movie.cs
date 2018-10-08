84b52e8fc73dfe0b2f2c062e9b7b6660bef11a80
Nelson Nyland <nelsonnyland@gmail.com>
2018-10-08 15:13:25 -0700

    Added object classes to Model folder. Added a folder for images.

---
+++ WorldwideMovieDatabase/Models/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorldwideMovieDatabase.Models
+{
+    public class Movie
+    {
+        // internal identifier
+        public int ID { get; set; }
+        
+        // Braveheart - Title
+        public string Title { get; set; }
+
+        // 1995 - Release Year
+        public DateTime ReleaseYear { get; set; }
+
+        // 8.4 - 861,254 users
+        public double UserRating { get; set; }
+
+        // R - Mature Audiences
+        public string MPAARating { get; set; }
+
+        // 2h 58min - movie runtime
+        public string MovieLength { get; set; }
+
+        // Drama - genre
+        public string Genre { get; set; }
+
+        // "When his secret bride is executed..."
+        public string Description { get; set; }
+
+        // Mel Gibson - Director
+        public string Director { get; set; }
+
+        // Mel Gibson, Sophie Marceau...
+        public string[] Actors { get; set; }
+    }
+}
---
+++ WorldwideMovieDatabase/Models/Profile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorldwideMovieDatabase.Models
+{
+    public class Profile
+    {
+        // internal identifier
+        public int ID { get; set; }
+
+        // Mel Gibson - First & Last Name
+        public string Name { get; set; }
+
+        // Living
+        public string BirthDeath { get; set; }
+
+        // Actor, Producer, Director
+        public string[] Titles { get; set; }
+
+        // "Mel Columcille Gerard Gibson..."
+        public string Bio { get; set; }
+
+        // "~/Images/..." - path for image
+        public string ProfilePicture { get; set; }
+    }
+}
