8ed8c7e714010099d57dfd55ebf72d44d26cd65e
Nelson Nyland <nelsonnyland@gmail.com>
2018-10-10 13:51:01 -0700

    Added code-first data annotations.

--- WorldwideMovieDatabase/Models/Movie.cs
+++ WorldwideMovieDatabase/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace WorldwideMovieDatabase.Models
     public class Movie
     {
         // internal identifier
+        [Key]
         public int ID { get; set; }
         
         // Braveheart - Title
--- WorldwideMovieDatabase/Models/Profile.cs
+++ WorldwideMovieDatabase/Models/Profile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace WorldwideMovieDatabase.Models
     public class Profile
     {
         // internal identifier
+        [Key]
         public int ID { get; set; }
 
         // Mel Gibson - First & Last Name
