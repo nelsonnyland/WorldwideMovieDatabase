4856894c116d5915d068e2b4c14748c9c0f3d101
Chad <chadd_84@yahoo.com>
2018-11-26 00:48:08 -0800

    Started working on GetProfileMovies

--- WorldwideMovieDatabase/Models/MovieDb.cs
+++ WorldwideMovieDatabase/Models/MovieDb.cs
@@ -13,5 +13,26 @@ namespace WorldwideMovieDatabase.Models
             db.Movies.AddRange(movies);
             db.SaveChanges();
         }
+
+
+        public static ICollection<Movie> GetProfileMovies(int? id)
+        {
+
+            //https://stackoverflow.com/questions/2767709/join-where-with-linq-and-lambda
+            //https://stackoverflow.com/questions/9720225/how-to-perform-join-between-multiple-tables-in-linq-lambda
+            var db = new ApplicationDbContext();
+            return db
+                .Profiles
+                .Join(db.MovieProfiles,
+                    p => p.ID,
+                    mp => mp.ProfileId,
+                    (p, mp) => new { p.ID, mp.MovieId })
+                .Join(db.Movies,
+                    mp => mp.MovieId,
+                    m => m.ID,
+                    (mp, m) => new { mp, m.Title })
+                .Where(p => p.ID == id)
+                .ToList();
+        }
     }
 }
