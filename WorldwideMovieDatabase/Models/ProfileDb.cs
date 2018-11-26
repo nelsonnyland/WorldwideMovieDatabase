bfd2099e8e56bb95498f5803d341b20eb57bee3a
Chad <chadd_84@yahoo.com>
2018-11-25 22:02:35 -0800

    Created ProfileDb with AddProfile method

---
+++ WorldwideMovieDatabase/Models/ProfileDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorldwideMovieDatabase.Models
+{
+    public static class ProfileDb
+    {
+        public static void AddProfile(Profile currProfile)
+        {
+            var db = new ApplicationDbContext();
+            db.Profiles.Add(currProfile);
+            db.SaveChanges();
+        }
+    }
+}
