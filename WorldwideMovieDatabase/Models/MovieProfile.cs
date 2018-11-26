981a0fdd462a16a6375c06a8a96c6e0b14458528
Chad <chadd_84@yahoo.com>
2018-11-25 22:10:56 -0800

    Added MovieProfile class to merge Movie and Profile with extra db column

---
+++ WorldwideMovieDatabase/Models/MovieProfile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WorldwideMovieDatabase.Models
+{
+    public class MovieProfile
+    {
+        [Key]
+        public int MovieProfileId { get; set; }
+        //[Key]
+        //[Column(Order = 0)]
+        public int MovieId { get; set; }
+
+        //[Key]
+        //[Column(Order = 1)]
+        public int ProfileId { get; set; }
+
+        public virtual Movie Movie { get; set; }
+
+        public virtual Profile Profile { get; set; }
+
+        public string JobTitle { get; set; }
+    }
+}
