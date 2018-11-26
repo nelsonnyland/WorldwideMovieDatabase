22c7b7edc5111d9fe3a96c96119f44bae21d6729
Chad <chadd_84@yahoo.com>
2018-11-25 22:01:09 -0800

    Created MovieDb with AddMovies method

---
+++ WorldwideMovieDatabase/Models/MovieDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorldwideMovieDatabase.Models
+{
+    public static class MovieDb
+    {
+        public static void AddMovies(ICollection<Movie> movies)
+        {
+            var db = new ApplicationDbContext();
+            db.Movies.AddRange(movies);
+            db.SaveChanges();
+        }
+    }
+}
