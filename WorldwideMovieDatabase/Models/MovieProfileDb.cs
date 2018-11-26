a11dfe26f48628b1439902b3805fbda341b2a58b
Chad <chadd_84@yahoo.com>
2018-11-25 22:01:44 -0800

    Created MovieProfileDb with AddMovieProfiles method

---
+++ WorldwideMovieDatabase/Models/MovieProfileDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorldwideMovieDatabase.Models
+{
+    public static class MovieProfileDb
+    {
+        public static void AddMovieProfiles(ICollection<MovieProfile> movieProfiles)
+        {
+            var db = new ApplicationDbContext();
+            db.MovieProfiles.AddRange(movieProfiles);
+            db.SaveChanges();
+        }
+    }
+}
