d70be0ca740086991075291423d8b4214e02904c
Chad <chadd_84@yahoo.com>
2018-11-28 00:04:17 -0800

    Removed method GetProfileMovies

--- WorldwideMovieDatabase/Models/MovieDb.cs
+++ WorldwideMovieDatabase/Models/MovieDb.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -12,32 +13,6 @@ namespace WorldwideMovieDatabase.Models
             var db = new ApplicationDbContext();
             db.Movies.AddRange(movies);
             db.SaveChanges();
-        }
-
-
-        public static List<Movie> GetProfileMovies(int? id)
-        {
-
-            //https://stackoverflow.com/questions/2767709/join-where-with-linq-and-lambda
-            //https://stackoverflow.com/questions/9720225/how-to-perform-join-between-multiple-tables-in-linq-lambda
-            var db = new ApplicationDbContext();
-            var query = db.Profiles
-                            .Where(p => p.ID == id)
-                            .Join(db.MovieProfiles,
-                                p => p.ID,
-                                mp => mp.ProfileId,
-                                (p, mp) => new { p, mp })
-                            .Join(db.Movies,
-                                pmp => pmp.mp.MovieId,
-                                m => m.ID,
-                                (pmp, m) => new { m.Title });
-
-            List<Movie> movieList = new List<Movie>();
-            foreach (var item in query)
-            {
-                movieList.Add(new Movie { Title = item.Title });
-            }
-            return movieList;
         }
     }
 }
